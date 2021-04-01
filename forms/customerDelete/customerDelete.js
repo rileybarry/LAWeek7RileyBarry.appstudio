e7a8669e4f43398ecf039e90d18666f572c181ee
rileybarry <60715866+rileybarry@users.noreply.github.com>
2021-03-31 21:52:26 -0500

    delete loop added

--- forms/customerDelete/customerDelete.js
+++ forms/customerDelete/customerDelete.js
@@ -10,9 +10,9 @@ customerDelete.onshow = function() {
 
   if (req.status == 200) { //transit trip worked. 
     results = JSON.parse(req.responseText)
-    console.log(`the results are \n ${results}`)
+    console.log(`${results}`)
     if (results.length == 0)
-      lblDelete.value = "There are no pets in the database."
+      lblDelete.value = "There are no customers in the database."
     else {
       let message = ""
       for (i = 0; i < results.length; i++)
@@ -22,4 +22,23 @@ customerDelete.onshow = function() {
 
   } else // the transit didn't work - bad wifi? server turned off?
     lblDelete.value = "Error code: " + req.status
+}
+
+btnDelete.onclick = function() {
+  let cusName = inptDelete.value
+  let strName = cusName.toString()
+  for (i = 0; i < results.length; i++) {
+  if (cusName == results[i][1]) {
+    query = `DELETE FROM customer WHERE name=${inptDelete.value}`
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+    if (req.status == 200) {
+      if (req.responseText == 500) {
+        lblDelete.value = `You have successfully deleted the customer named ${cusName}`
+      } else {
+        lblDelete.value = `There was a problem deleting ${cusName} from the database.`
+      }
+    } else {
+      lblDelete.value = "Error code: " + req.status
+    }
+  }
 }
--- forms/customerSelect/customerSelect.js
+++ forms/customerSelect/customerSelect.js
@@ -13,7 +13,7 @@ customerSelect.onshow = function() {
     results = JSON.parse(req.responseText)
     console.log(`the results are \n ${results}`)
     if (results.length == 0)
-      lblMessage.value = "There are no pets in the database."
+      lblMessage.value = "There are no customers in the database."
     else {
       let message = ""
       for (i = 0; i < results.length; i++)
