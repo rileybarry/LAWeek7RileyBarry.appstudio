99dc06d024f0dcd743b9e98ca1ba8e6548354554
Riley Barry <rwb12128@creighton.edu>
2021-05-05 16:05:16 -0500

    Complete delete

--- forms/customerDelete/customerDelete.js
+++ forms/customerDelete/customerDelete.js
@@ -10,7 +10,7 @@ customerDelete.onshow = function() {
 
   if (req.status == 200) { //transit trip worked. 
     results = JSON.parse(req.responseText)
-    console.log(`${results}`)
+    
     if (results.length == 0)
       lblDelete.value = "There are no customers in the database."
     else {
@@ -30,17 +30,10 @@ btnDelete.onclick = function() {
     if (cusName == results[i][1]) {
       query = "DELETE FROM customer WHERE name= '" + cusName + "'"
       req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
-      if (req.status == 200) {
-        if (req.responseText == 500) {
-          lblDelete.value = `You have successfully deleted the customer named ${cusName}`
-        } else {
-          lblDelete.value = `There was a problem deleting ${cusName} from the database.`
-        }
-      } else {
-        lblDelete.value = 'The customer is not in the database'
-      }
-    } else {
-      lblDelete.value = "Error code: " + req.status
+      if (req.status == 200)
+        lblDelete.value = `You have successfully deleted the customer named ${cusName}`
+      else
+        lblDelete.value = "Error code: " + req.status  
     }
   }
 }
